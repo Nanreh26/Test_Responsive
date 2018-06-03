6d193cf2da099b569468e7f2dd0701efed54d27d
Ivan Aguilar (Nanreh) <nanreh2694@gmail.com>
2018-06-02 19:37:23 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementByID('form')
+
+id(form){
+  form.addEventListener('submit', () => {
+    let width = form.querySelector('#width'),
+        height = form.querySelector('#height'),
+        url = form.querySelector('#url'),
+        
+    if(width && height && url){
+      width = width.value
+      height = height.value
+      url = url.value
+    }
+    
+    window.open(url, url, 'innerWidth=${width},innerHeight=${height}')
+  })
+}
